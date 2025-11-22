66f37d501004491fb31de9711b0d50442de4362d
admin12121 <vickytaj6459@gmail.com>
2025-11-22 18:39:53 +0545

    stylesheet issue fixed and server code updated

--- app/root.tsx
+++ app/root.tsx
@@ -2,6 +2,7 @@ import {
   isRouteErrorResponse,
   Outlet,
   Scripts,
+  Links,
   ScrollRestoration,
 } from "react-router";
 import { RootProvider } from "fumadocs-ui/provider/react-router";
@@ -55,6 +56,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>Admin12121</title>
+          <Links />
         </head>
         <body className="flex flex-col min-h-screen">
           <Navbar />
